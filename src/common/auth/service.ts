1039d628355360a32766d7cd6831f542e62110db
Tahmid Sadik <tahmidsadik112@gmail.com>
2020-04-07 02:59:10 +0600

    Adds authentication modules
    - Adds auth related functions
    - Uses bcrypt for password hashing
    - Adds functions for generating and verifying access tokens

--- src/common/auth/service.ts
+++ src/common/auth/service.ts
@@ -1,29 +1,68 @@
 import { v4 as uuidV4 } from 'uuid';
 import { orm } from '../../db';
-import { hash } from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 import { log } from '../../server';
 import { findUserById } from '../../user/service';
-import { UserAccessTokens as UserAccessToken } from '../../entities';
+import { UserAccessTokens as UserAccessToken, User } from '../../entities';
 
 /*
  * * in a separate function so that we can control the generation logic separately
  */
 async function genToken(): Promise<string> {
-  const token = await hash(uuidV4(), 12);
-  log.info('generated token = ', token);
-  return token;
+  return uuidV4();
 }
 
 export async function generateAndAssociateAccessTokenWithUser(
-  userID: number
-): Promise<void> {
-  const user = await findUserById(userID);
+  user: number | User
+): Promise<UserAccessToken> {
+  let userEntity: User | null;
+
+  if (typeof user === 'number') {
+    userEntity = await findUserById(user);
+    if (!userEntity) {
+      throw Error('Invalid UserID');
+    }
+  } else if (user instanceof User) {
+    userEntity = user;
+  } else {
+    throw Error('Given user has invalid type ');
+  }
   const token = await genToken();
 
   const uat = new UserAccessToken({
-    user,
+    user: userEntity,
     token,
   });
 
-  orm.em.persistAndFlush(uat);
+  await orm.em.persistAndFlush(uat);
+  return uat;
+}
+
+export async function verifyAccessToken(
+  userID: number,
+  token: string
+): Promise<boolean> {
+  const user = await orm.em.findOne(UserAccessToken, {
+    user: {
+      id: userID,
+    },
+    token,
+  });
+  return !!user;
+}
+
+export async function hashPassword(password: string): Promise<string> {
+  return hash(password, 12);
+}
+
+export async function matchPassword(
+  userID: number,
+  password: string
+): Promise<boolean> {
+  const user = await findUserById(userID);
+  if (!user) {
+    return false;
+  }
+  log.debug(user);
+  return compare(password, user.password);
 }
